953476cbd7bafe9bf674b15e19382d9f0fa25575
Gokhan Akpinar <gokhanakpinar@Gokhans-MacBook-Pro.local>
2024-09-02 09:09:22 +0300

    Refactoring Order domain layer: Adding Outbox schedulers for Approval

--- order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/ports/output/repository/ApprovalOutboxRepository.java
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/ports/output/repository/ApprovalOutboxRepository.java
@@ -1,14 +1,15 @@
 package com.food.ordering.system.order.service.domain.ports.output.repository;
 
 import com.food.ordering.system.order.service.domain.outbox.model.approval.OrderApprovalOutboxMessage;
-import com.food.ordering.system.order.service.domain.outbox.model.payment.OrderPaymentOutboxMessage;
 import com.food.ordering.system.outbox.OutboxStatus;
 import com.food.ordering.system.saga.SagaStatus;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface ApprovalOutboxRepository {
 
     OrderApprovalOutboxMessage save(OrderApprovalOutboxMessage orderApprovalOutboxMessage);
